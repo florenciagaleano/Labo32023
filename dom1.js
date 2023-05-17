59e0fdc431b84f1280084ad80dd623bcd073ba9e
Alumno <alumno@sistemas-utnfra.com>
2023-05-16 22:04:27 -0300

    Ver lo ultimo

--- dom1.js
+++ dom1.js
@@ -54,7 +54,7 @@ $tarjeta1.classList.remove("sepia");
 $tarjeta1.classList.toggle("sepia");//si no lo tengo lo agrega y si lo tengo lo saca
 console.log($tarjeta1.classList.contains("sepia"));
 */
-
+/*
 $parrafo.textContent = "<mark>Estoy desde js</mark>";//no interpreta html, el text content
 $parrafo.innerHTML = `<p><mark>Estoy desde js</mark></p>
 <p><mark>Estoy desde js</mark></p>
@@ -66,3 +66,52 @@ $parrafo.outerHTML = `<p><mark>Estoy desde js</mark></p>
 <p><mark>Estoy desde js</mark></p>`;
 
 $parrafo.querySelector("p");
+
+const $newTarjeta = document.createElement("figure"),
+$imagen = document.createElement("img"),
+$fig = document.createElement("figcaption"),
+texto = document.createTextNode("Any"),
+$sectionImagenes = document.querySelector(".imagenes");
+
+$newTarjeta.classList.add("tarjeta");
+$imagen.setAttribute("src","https://placeimg.com//200/200/any")
+$imagen.setAttribute("alt","cualquiera");
+$newTarjeta.appendChild($imagen);
+$newTarjeta.appendChild($fig);
+$fig.appendChild(texto);
+
+$sectionImagenes.appendChild($newTarjeta);
+*/
+/*
+const listaFrutas = ['banana','manzana','pera','melon','sandia'];
+$listaFrutas = document.getElementById("frutas");
+const fragmento = document.createDocumentFragment();
+//$listaFrutas = document.createElement("li");
+listaFrutas.forEach((el)=>{
+    const $li = document.createElement("li");
+    $li.textContent = el;
+    fragmento.appendChild($li);
+})
+
+$listaFrutas.appendChild(fragmento);*/
+const $imagenes = document.querySelector(".imagenes");//section imagenes
+let $tarjeta = $imagenes.firstElementChild;
+//console.log($tarjeta);
+//me muestra uno y tendria q mostrar 4
+//conseguir un array de objetos bajarlo como json pasarloa  una variable array de esos objetos, tirarlo a funcion que devuelta la tabla cargada con lso elementos y les lea las keys.
+//Por ej si hay algo con id que lo lea
+//se hace la tr del encabezado con las keys y el resto del contenido con los valores de losobjetos
+do{
+    console.log($tarjeta);
+    $tarjeta = $tarjeta.nextElementSibling;
+}while($tarjeta.nextElementSibling)
+//console.log($imagenes);
+
+
+
+
+
+
+
+
+
